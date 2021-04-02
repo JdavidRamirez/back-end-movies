49f1d9e5c56c5b3d3e7c5098927c498a5d5a7843
JdavidRamirez <entropiadeluniverso@gmail.com>
2021-04-02 16:26:33 -0500

    Third commit update project

--- src/main/java/co/edu/iudigital/app/service/iface/MovieService.java
+++ src/main/java/co/edu/iudigital/app/service/iface/MovieService.java
@@ -5,7 +5,32 @@ import java.util.List;
 import co.edu.iudigital.app.model.entity.Movie;
 
 public interface MovieService {
-	//Lista de todas las películas
+	/**
+	 * Get all films
+	 * @return
+	 */
 	public List<Movie> getAll();
+	/**
+	 * Create a film
+	 * @param
+	 */
+	public void create(Movie movie);
+	/**
+	 * Edit a movie by his Id
+	 * @param movie
+	 * @param movieId
+	 */
+	public void edit(Movie movie, int movieId);
+	/**
+	 * Erase a movie by his Id
+	 * @param movieId
+	 */
+	public void delete(int movieId);
+	/**
+	 * Califica una película
+	 * @param movie
+	 * @param movieId
+	 */
+	public void rate(Movie movie, int movieId);
 
 }
--- src/main/java/co/edu/iudigital/app/service/impl/MovieServiceImpl.java
+++ src/main/java/co/edu/iudigital/app/service/impl/MovieServiceImpl.java
@@ -1,26 +1,79 @@
 package co.edu.iudigital.app.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+
 import co.edu.iudigital.app.model.entity.Movie;
+import co.edu.iudigital.app.model.repository.GenderRepository;
 import co.edu.iudigital.app.model.repository.MovieRepository;
 import co.edu.iudigital.app.service.iface.MovieService;
 
 @Service
-public class MovieServiceImpl implements MovieService{
+public class MovieServiceImpl implements MovieService {
+	
 	@Autowired
 	private MovieRepository movieRepository;
 	
+	@Autowired
+	private GenderRepository genderRepository;
+	
 	@Override
-	public List<Movie> getAll(){
-		List<Movie> movies= new ArrayList<>();
-		movies =(List<Movie>) movieRepository.findAll();
+	public List<Movie> getAll() {
+		List<Movie> movies = new ArrayList<>();
+		movies = (List<Movie>) movieRepository.findAll();
 		return movies;
 	}
 	
+	@Override
+	public void create(Movie movie) {
+		boolean existsGender = genderRepository
+				.existsById(movie.getGenderId().getId());
+		if(existsGender) {
+			movie.setCreateAt(LocalDateTime.now());
+			movieRepository.save(movie);
+		}
+	}
+	
+	@Override
+	public void edit(Movie movie, int movieId) {
+		Optional<Movie> existsMovie = movieRepository.findById(movieId);
+		if(existsMovie.isPresent()) {
+			existsMovie.get().setNAME(movie.getNAME());
+			existsMovie.get().setDESCRIPTION(movie.getDESCRIPTION());
+			existsMovie.get().setACTORS(movie.getACTORS());
+			existsMovie.get().setIMAGE(movie.getIMAGE());
+			existsMovie.get().setRATING(movie.getRATING());
+			existsMovie.get().setReleaseDate(movie.getReleaseDate());
+			boolean existsGender = genderRepository
+					.existsById(movie.getGenderId().getId());
+			if(existsGender) {
+				existsMovie.get().setGenderId(movie.getGenderId());
+			}
+			movieRepository.save(existsMovie.get());
+		}
+	}
+	
+	@Override
+	public void rate(Movie movie, int movieId) {
+		Optional<Movie> existsMovie = movieRepository.findById(movieId);
+		if(existsMovie.isPresent()) {
+			existsMovie.get()
+			.setRATING(existsMovie.get().getRATING() + movie.getRATING());
+			movieRepository.save(existsMovie.get());
+		}
+	}
+
+	@Override
+	public void delete(int movieId) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
